e10bb7c4886ad65c225e573efa710f36a6d224c1
Mallikavempatapu <97509474+Mallikavempatapu@users.noreply.github.com>
2022-01-27 13:30:49 +0530

    Add files via upload

---
+++ step2/ProtectiveAccessspecifiers.java
@@ -0,0 +1,9 @@
+package AccessSpecifier.Package2;
+
+public class ProtectiveAccessspecifiers {
+	public void display() 
+    { 
+        System.out.println("This is protected access specifier"); 
+    } 
+
+}
